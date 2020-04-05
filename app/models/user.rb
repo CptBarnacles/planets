47425afe78678de1bf895c9ad99ce921debb29d0
Alex Whyatt <59887470+CptBarnacles@users.noreply.github.com>
2020-04-05 15:39:41 +0100

    keep trying150

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,4 @@ class User < ApplicationRecord
                    uniqueness: { case_sensitive: false }
 has_secure_password
 validates :password, presence: true, length: { minimum: 6}
-	devise :timeoutable
 end
